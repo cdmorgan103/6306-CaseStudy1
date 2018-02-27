37a6811c6a87321fe08ae5579e301e94e85bd2ad
Chris Morgan <cdmorgan@mail.smu.edu>
2018-02-26 23:47:50 -0600

    push for finalization, rework on brewries q1 for errors

--- practice area.R
+++ practice area.R
@@ -1,3 +1,6 @@
+getwd
+setwd()
+
 beers <- read.csv("Beers.csv")
 breweries <-read.csv("Breweries.csv")
 
@@ -10,15 +13,6 @@ colnames(breweries)[2] <- c("brewerCompany")
 #Rename Brewery_id to Brew_id in beers data, same as in breweries in order to merge data sets
 colnames(beers)[5] <- c("Brew_ID")
 
-#Select only Beers regarless of the Beer_ID or bottled Presentation. This will only give all produced beers.
-beersOnly <- beers[c(-2,-7)]
-
-#Review duplicate Beers. There are 78 records showing extra records for existing beers.
-beersOnlyDup <- beersOnly[duplicated(beersOnly),]
-
-#Select Unique Beers after Validating that all the Duplication are valid. However, there are still seem to be duplicates of the beers, due to the year that was produced. For this exercise beers produced on different years will be consider different, and not duplicated.
-# A unique beer is unique on beerName, ABV, IBU, Brewery, and Style. The Beer_ID and the Ounces were removed.
-uniqueBeers <- beersOnly[!duplicated(beersOnly),]
 
 # Trim the leading white-spce from the State
 breweries$State <- trimws(breweries$State)
@@ -30,7 +24,7 @@ stateNames <- data.frame(stateName=c(state.name,"Washington DC"),State=c(state.a
 # add the State Name to the Breweries Data Frame
 breweries <- merge(breweries,stateNames,by="State", all=TRUE)
 
-
+uniqueBreweries <- breweries[!duplicated(breweries[c(3,4,5)]),]
 
 
 ################Brewery cleanse
@@ -43,13 +37,21 @@ breweries <- merge(breweries,stateNames,by="State", all=TRUE)
 ##########################################################################
 
 
+#Merge beers and breweries data together
+m1 <- merge(beers,breweries,by="Brew_ID")
 
+
+
+#performs search for parenthesis values, removes them from a nondestructive column for beers
 m1$beerNameNoY<-gsub("[:(:].*[:):]","",m1$beerName)
+#trims white space around newly trimmed data for beers
 m1$beerNameNoY<-trimws(m1$beerNameNoY)
+#performs search for parenthesis values, removes them from a nondestructive column for brewries
 m1$brewerNameNoP<-gsub("[:(:].*[:):]","",m1$brewerCompany)
+#trims white space around newly trimmed data for brewries
 m1$brewerNameNoP<-trimws(m1$brewerNameNoP)
 
-
+#eliminates duplicates for beers
 m1 <- m1[!duplicated(m1[,c("beerNameNoY","brewerNameNoP","ABV","IBU","Style")]),]
 
 
@@ -62,13 +64,16 @@ m1 <- m1[!duplicated(m1[,c("beerNameNoY","brewerNameNoP","ABV","IBU","Style")]),
 
 
 #q1 how many breweries in US???
-countBrewByState <- table(breweries$stateName)
+uniqueBreweries <- uniqueBreweries[!duplicated(uniqueBreweries[,c("brewerCompany","City","stateName")]),]
+countBrewByState <- table(uniqueBreweries$stateName)
 countBrewByState <- as.data.frame(countBrewByState)
 colnames(countBrewByState)=c("stateName","BreweriesCount")
 countBrewByState
 
-#Merge beers and breweries data together
-m1 <- merge(beers,breweries,by="Brew_ID")
+countBrewByState2 <- table(uniqueBreweries$stateName)
+countBrewByState2 <- as.data.frame(countBrewByState2)
+colnames(countBrewByState2)=c("stateName","BreweriesCount")
+countBrewByState2
 
 #Table with the first 6 observations from m1 (merged data)
 head(m1)
